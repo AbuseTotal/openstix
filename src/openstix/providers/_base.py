c037ac0cde62917c24c557646ccacc19df0cbf85
Pedro Henrique Fernandes <pedro007henrique41@gmail.com>
2024-06-30 22:49:11 +0000

    feat(dataset): implement internal caching using MemoryStore for query_one method

--- src/openstix/providers/_base.py
+++ src/openstix/providers/_base.py
@@ -1,6 +1,7 @@
 from abc import ABC
 
 from pydantic import BaseModel
+from stix2 import MemoryStore
 
 from openstix.filters import Filter
 from openstix.toolkit import Environment
@@ -18,18 +19,24 @@ class Dataset(ABC):
 
     def __init__(self, source: DataSource):
         self.environment = Environment(source=source)
+        self.cache = MemoryStore()
 
     def _query(self, filters=None):
         filters = filters if filters else []
         return self.environment.query(filters)
 
     def _query_one(self, filters=None):
-        filters = filters if filters else []
+        filters: list[Filter] = filters if filters else []
+
+        cached_objects = self.cache.query(filters)
+        if cached_objects:
+            return cached_objects[0]
 
         objects = self._query(filters)
-
         if objects:
-            return objects[0]
+            obj = objects[0]
+            self.cache.add([obj])
+            return obj
 
         return None
 
